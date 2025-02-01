3a24544b5a85f4eba264a8206936a6527e01d036
unknown <cristian.palaghiei@yahoo.com>
2025-02-01 18:26:29 +0200

    prod

--- src/components/SelectedPlan.jsx
+++ src/components/SelectedPlan.jsx
@@ -1,10 +1,11 @@
 import styles from './SelectedPlan.module.css'
+import musicIcon from '../../public/icon-music.svg'
 
 function SelectedPlan() {
     return (
         <div className={styles.selectedPlan}>
             <div className={styles.planPreview}>
-                <img src="../../images/icon-music.svg" alt="Music image" />
+                <img src={musicIcon} alt="Music image" />
                 <div className={styles.planType}><p className={styles.planTypeText}>Annual Plan</p><p className={styles.price}>$59.99/year</p></div>
             </div>
             <button class={styles.changeButton}>Change</button>
--- src/components/SummaryHero.jsx
+++ src/components/SummaryHero.jsx
@@ -1,9 +1,10 @@
 import styles from './SummaryHero.module.css'
+import headerImage from '../../public/illustration-hero.svg'
 
 function SummaryHero() {
     return (
         <>
-            <img className={`${styles.heroImage}`} src="../../images/illustration-hero.svg" alt="Hero Image" />
+            <img className={`${styles.heroImage}`} src={headerImage} alt="Hero Image" />
         </>
     )
 }
